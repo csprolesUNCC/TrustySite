798349173b34a0f6d66a913eaacf5adb1331e804
csprolesUNCC <csproles@charlotte.edu>
2025-12-05 13:40:31 -0500

    Final login fix

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import connectToDatabase from '../connect.js';
-import { authenticateUser } from '/utils/auth.js';
+import { authenticateUser } from '../../utils/auth.js';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET) {
